bfd2e9aa324f17631625845b472a33b1421e26c4
張皓雨 <39931469+321qwedsa000@users.noreply.github.com>
2020-04-18 21:55:24 +0800

    MergeSort Complte

--- Algorithm/MergeSort.py
+++ Algorithm/MergeSort.py
@@ -1,26 +1,28 @@
-from Merge import merge
-
-class MergeSortBase:
-    def m(self,ls: list,f:int,m:int,l:int):
-        left = ls[f:m]
-        right = ls[m:]
-        
-    @staticmethod
-    def mergeSort(ls : list, f: int, l: int):
-        if f < l:
-            o = MergeSortBase()
-            mid = (f+l)//2
-            MergeSortBase.mergeSort(ls,f,mid)
-            MergeSortBase.mergeSort(ls,mid+1,l)
-            o.m(ls,f,mid,l)
+from random import Random
 
 def mergeSort(ls : list):
-    first = 0
-    last = len(ls)-1
-    MergeSortBase.mergeSort(ls,first,last)
+    if len(ls) > 1:
+        mid = len(ls) // 2
+        l = ls[:mid]
+        r = ls[mid:]
+        mergeSort(l)
+        mergeSort(r)
+        i = j = k = 0
+        while i < len(l) or j < len(r):
+            if j == len(r) or i < len(l) and l[i] < r[j]:
+                ls[k] = l[i]
+                i += 1
+            else:
+                ls[k] = r[j]
+                j += 1
+            k += 1
 
 
 if __name__ == "__main__":
-    a = [1,4,5,3,2,7,6,8]
+    SEED = 0xF1
+    a = [x for x in range(1,21)]
+    rndEngine = Random(SEED)
+    rndEngine.shuffle(a)
+    print(a)
     mergeSort(a)
     print(a)
